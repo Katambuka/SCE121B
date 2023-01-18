7fb492cc11930d8950af751766b75c2a7199b36f
Katambuka <kan22001@byui.edu>
2023-01-18 12:20:11 +0400

    edited

---
+++ scripts/operators.js
@@ -0,0 +1,32 @@
+// operators.js
+let shipHealth = 3;
+let shipAmmo = 3;
+let targetHealth = 3;
+
+function fireShip() {
+  if (shipCanFire()) {
+    shipAmmo--;
+    if (isHit()) {
+      targetHealth--;
+      console.log("hit - targetHealth:", targetHealth);
+    } else {
+      console.log("miss");
+    }
+  } else {
+    reloadShip();
+    console.log("reloading, shipHealth:", shipHealth);
+  }
+}
+
+function encounter() {
+  console.log("You see a target");
+  if (!isDestroyed(targetHealth) && !isDestroyed(shipHealth)) {
+    fireShip();
+    if (isDestroyed(targetHealth)) {
+      console.log("Target eliminated");
+    }
+    if (isDestroyed(shipHealth)) {
+      console.log("ship destroyed");
+    }
+  }
+}
