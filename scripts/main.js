ddc3fdfe56c33b3e8530dfdc66ed3dc5ab3154a8
Katambuka <kan22001@byui.edu>
2023-01-22 22:55:40 +0400

    edited

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+
+
 // toggle menu in small view
 const toggleMenu = () => {
     document.querySelector('#menu').classList.toggle('open');
