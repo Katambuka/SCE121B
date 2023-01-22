ba285f7d07a6dd79636042b787ddfb18b7cba212
Katambuka <kan22001@byui.edu>
2023-01-23 00:36:01 +0400

    ttt

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,54 @@ document.querySelector('#year').textContent = currentDate.getFullYear();
 document.querySelector("html").addEventListener("click", function () {
     alert("Ouch! Stop poking me!");
   });
+  
+  
+  let myButton = document.querySelector("button");
+  let myHeading = document.querySelector("h1");
+  
+  
+  
+  // adding image in js
+  const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+    ///Creating and placing new nodes
+    const link = document.querySelector('a');
+    link.textContent = 'Mozilla Developer Network';
+    link.href = 'https://developer.mozilla.org';
+    const sect = document.querySelector('section');
+    const para = document.createElement('p');
+    para.textContent = 'We hope you enjoyed the ride.';
+    sect.appendChild(para);
+    
+    /// let's add a text node to the paragraph the link sits inside
+    const text = document.createTextNode(' — the premier source for web development knowledge.');
+    
+    //grab a reference to the paragraph
+    const linkPara = document.querySelector('p');
+    linkPara.appendChild(text);
+    
+    ///adding and removing
+    //sect.appendChild(linkPara);
+    ///linkPara.remove();
+    ///linkPara.parentNode.removeChild(linkPara);
+    
+    
+    ///Manipulating styles
+    para.style.color = 'white';
+    para.style.backgroundColor = 'black';
+    para.style.padding = '10px';
+    para.style.width = '250px';
+    para.style.textAlign = 'center';
+    
+    para.setAttribute('class', 'highlight');
+    
+    ///A dynamic shopping list
